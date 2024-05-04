75a7e95a37f43bcab907b9c4471f697a61ce3229
cl1107 <cl_1107@126.com>
2024-05-04 23:00:35 +0800

    添加github action

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       js: process.env.NODE_ENV === 'production' ? false : 'source-map',
       css: false,
     },
+    assetPrefix: '/rspack-rsbuild-react-antd-admin-pro',
   },
   source: {
     define: {
